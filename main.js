1825724e5561d129d2c3a1d09a17401adcf12a33
erii <eliza.m.jma@gmail,com>
2022-12-26 00:06:02 +0100

    skybox and plane

--- main.js
+++ main.js
@@ -5,12 +5,40 @@ camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight,
 renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-camera.position.set (0, 0, 5);
+camera.position.set (0, 0, 100);
 
 //TrollbackControls
 const controls = new THREE.TrackballControls(camera, renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
+//skyBox - City
+scene.background = new THREE.CubeTextureLoader().load([
+    "skybox/north.jpg",
+    "skybox/south.jpg",
+    "skybox/top.jpg",
+    "skybox/bottom.jpg",
+    "skybox/west.jpg",
+    "skybox/east.jpg"
+]);
+
+//plane - city
+const planeLoader = new THREE.TextureLoader();
+const planeTexture = planeLoader.load('plane/plane.jpeg');
+planeTexture.repeat.set(16, 16);
+planeTexture.wrapS = THREE.RepeatWrapping;
+planeTexture.wrapT = THREE.RepeatWrapping;
+planeTexture.anisotropy = 16;
+const planeGeometry = new THREE.PlaneGeometry(1000, 1000, 1000, 1000);
+const planeMaterial = new THREE.MeshBasicMaterial({map: planeTexture});
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+plane.rotation.x = - Math.PI / 2;
+plane.position.set(0, -50, 0);
+plane.castShadow = false;
+plane.receiveShadow = true;
+scene.add(plane);
+
+
+//cube
+const geometry = new THREE.BoxGeometry(5, 5, 5);
 const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
@@ -18,9 +46,6 @@ scene.add(cube);
 function animate() {
     requestAnimationFrame(animate);
     controls.update();
-    // cube.rotation.x += 0.01;
-    // cube.rotation.y += 0.01;
-
     renderer.render(scene, camera);
 }
 
